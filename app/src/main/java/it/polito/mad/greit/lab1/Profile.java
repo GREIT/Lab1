11cb05fb50f52bc3ccc1c72a629c198a64604e43
Fabio Cermelli <f.cermelli94@gmail.com>
2018-03-19 10:43:53 +0100

    Added UI

---
+++ app/src/main/java/it/polito/mad/greit/lab1/Profile.java
@@ -0,0 +1,15 @@
+package it.polito.mad.greit.lab1;
+
+/**
+ * Created by fcerm on 19/03/2018.
+ */
+
+public class Profile {
+    private String name;
+    private String surname;
+    private String email;
+
+
+
+
+}
--- app/src/main/java/it/polito/mad/greit/lab1/showProfile.java
+++ app/src/main/java/it/polito/mad/greit/lab1/showProfile.java
@@ -2,6 +2,7 @@ package it.polito.mad.greit.lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class showProfile extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class showProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_profile);
+
+        Button save = findViewById(R.id.save);
+     //   save.setOnClickListener( v -> createProfile() );
     }
+
+
+
 }
