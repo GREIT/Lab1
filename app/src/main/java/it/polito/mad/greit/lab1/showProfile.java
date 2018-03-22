47b1244309990afafcd882ed02d87219a162216b
carminedamico <Carmine920@gmail.com>
2018-03-22 19:02:47 +0100

    First commit

--- app/src/main/java/it/polito/mad/greit/lab1/showProfile.java
+++ app/src/main/java/it/polito/mad/greit/lab1/showProfile.java
@@ -2,12 +2,16 @@ package it.polito.mad.greit.lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class showProfile extends AppCompatActivity {
+    Profile P;
+    TextView T;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_profile);
+        
     }
 }
